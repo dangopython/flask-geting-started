d10a2bb31792fe3923fd6ed66c44d8ed321dd11e
Sagar Pohekar <sagarpohekar@gmail.com>
2023-12-06 23:09:54 +0530

    1. Parameters in URLs 2. Returning 404 error when resource is absent 3. Reverse URL building using `url_for` 4. Jinja For loops 5. Jinja if-else-endif construct 6. Serving data as REST API
    Summary:
    
    Card detail page
      - Parameter in URL to select card
      - Return 404 when not found
      - Creating links with url_for
      - If statement in card template
    
    Card list
      - For loop in template
    
    Serving data as REST API

--- flashcards.py
+++ flashcards.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, abort, jsonify
 from model import db
 
 app = Flask(__name__)
@@ -6,10 +6,33 @@ app = Flask(__name__)
 
 @app.route('/')
 def welcome():
-    return render_template("welcome.html", message="- Sagar")
+    return render_template(
+        "welcome.html",
+        cards=db)
 
 
-@app.route('/card')
-def card_view():
-    card = db[0]
-    return render_template("card.html", card=card)
+@app.route('/card/<int:index>')
+def card_view(index):
+    try:
+        card = db[index]
+        return render_template("card.html",
+                               card=card,
+                               index=index,
+                               max_index=len(db) - 1)
+    except IndexError:
+        abort(404)
+
+
+# REST API
+
+@app.route("/api/card/")
+def api_card_list():
+    return jsonify(db)
+
+
+@app.route('/api/card/<int:index>')
+def api_card_detail(index):
+    try:
+        return db[index]
+    except IndexError:
+        abort(404)
