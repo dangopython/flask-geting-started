0acac413aad5de36cac4cceba8226a34b3d67798
Sagar Pohekar <sagarpohekar@gmail.com>
2023-12-06 21:39:20 +0530

    flask getting started

---
+++ flashcards.py
@@ -0,0 +1,24 @@
+from datetime import datetime
+from flask import Flask
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def welcome():
+    return "Welcome to my Flash Cards application!"
+
+
+@app.route('/date')
+def date():
+    return f"This page was served at {datetime.now()}"
+
+
+counter = 0
+
+
+@app.route("/count_views")
+def count_demo():
+    global counter
+    counter += 1
+    return f"This page was served {counter} times"
